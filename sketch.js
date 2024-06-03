9c2075cb140a2c8b73e0d29369feff4e9198a1b0
yyyzc122 <150638485+yyyzc122@users.noreply.github.com>
2024-06-03 12:21:19 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -90,8 +90,8 @@ function drawSkeleton() {
     if (partA.score > 0.1 && partB.score > 0.1) {
       //line(partA.x, partA.y, partB.x, partB.y);
       push()
-       image(car.gif,partA.x-75,partA.y-75,150,150)
-       image(car.gif,partB.x-75,partB.y-75,150,150)
+       image(carImg,partA.x-75,partA.y-75,150,150)
+       image(carImg,partB.x-75,partB.y-75,150,150)
       pop()
       
     }
